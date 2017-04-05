389a7871a3264c7f54f12aeb7b924c8c92ffa092
cristinamisas <cristinamisas099@gmail.com>
2017-04-05 15:56:03 -0700

    WIP ajax

--- scripts.js
+++ scripts.js
@@ -1,1 +1,18 @@
+$(document).ready(function(){
+  $('#contacts_link').on('click', function(event) {
+      event.preventDefault();
+ // Event handler code goes here
+})
+var $ajax = $.ajax({
+        url : 'https://gdi-dbc-sd.herokuapp.com/contacts',
+        method: 'GET'
+        datatype: 'HTML'
+    })
+      $ajax.done(function(response){
+      console.log(response);
 
+})
+
+  })
+
+})
