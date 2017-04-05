996083263f9462e4b77fe19292278b5cced18464
Lili <301745@guhsd.net>
2017-04-05 16:23:17 -0700

    all done

--- scripts.js
+++ scripts.js
@@ -1,1 +1,25 @@
+$(document).ready(function(){
 
+    $('#contacts_link').on('click', function(event) {
+        event.preventDefault()
+        $('#contacts_link').text("click here!!")
+        var $ajax = $.ajax({
+          url: "https://gdi-dbc-sd.herokuapp.com/contact",
+          method: 'GET',
+          dataType: 'HTML',
+
+        //  context: document.body
+        }).done(function(response) {
+        //  alert(response)
+        var $body = $('body')
+          $body.append(response)
+        });
+
+
+        $ajax.fail(function(){
+          alert ("error");
+        })
+
+    })
+
+  })
