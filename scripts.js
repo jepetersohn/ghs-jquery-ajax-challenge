0448b7584d2990e3eb02fb6e6305f4553e7c4a06
Nathan Jones <nathanjones731@gmail.com>
2017-04-05 15:50:07 -0700

    Complete release 1-5

--- scripts.js
+++ scripts.js
@@ -1,1 +1,28 @@
+$(document).ready(function(){
+  var counter = 0
 
+    $("#contacts_link").on("click", function(event){
+      event.preventDefault()
+
+
+      if(counter<1) {
+        var $ajax = $.ajax({
+          url: $("#contacts_link").attr('href'),
+          method: 'GET'
+        })
+
+        $ajax.done(function(response) {
+            $("body").append(response)
+            console.log(response)
+        })
+
+        $ajax.fail(function() {
+            $("body").append("Something went wrong")
+            console.log("Failed")
+        })
+      }
+        counter++
+    })
+
+
+})
