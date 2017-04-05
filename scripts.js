26334351354f8db9173895f1254eba514c4689a6
Chris <willisnjchris@yahoo.com>
2017-04-05 16:09:14 -0700

    final commit

--- scripts.js
+++ scripts.js
@@ -1,1 +1,20 @@
-
+$(document).ready(function(){
+  $('#contacts_link').on('click', function(event) {
+     event.preventDefault();
+     var $ajax = $.ajax({
+        url: $(this).attr('href'),
+        method: 'GET',
+        //dataType: 'JSON'
+     })
+     var ajaxTime= new Date().getTime();
+     $ajax.done( function(response) {
+       console.log(response)
+       var totalTime = new Date().getTime()-ajaxTime;
+       $("body").append(response).prepend(totalTime)
+     })
+     $ajax.fail( function(xhr, ajaxOptions, thrownError) {
+       console.log(thrownError)
+       $("body").append(thrownError)
+     })
+  });
+})
