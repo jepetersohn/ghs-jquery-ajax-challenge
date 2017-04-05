6c1f5a0ecc4184e9e59843208a8e583d18bb2d1b
justinuranga <299693@guhsd.net>
2017-04-05 16:12:00 -0700

    completes challenges

--- scripts.js
+++ scripts.js
@@ -1,1 +1,14 @@
-
+$(document).ready(function(){
+  $('#contacts_link').on('click', function(event) {
+    event.preventDefault();
+    var $ajax = $.ajax({
+    url: "https://gdi-dbc-sd.herokuapp.com/contacts" ,
+    method: 'GET',
+    dataType: 'HTML'
+  })
+   $ajax.done(function(response){
+    console.log(response)
+    $(response).insertAfter('a')
+   })
+})
+})
